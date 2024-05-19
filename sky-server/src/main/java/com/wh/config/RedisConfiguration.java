9c35f5b7c9a0b3d3db8371b3343ebaf6895f50ea
WH934426 <wh934426@outlook.com>
2024-05-19 22:33:13 +0800

    perf: 添加redis配置

---
+++ sky-server/src/main/java/com/wh/config/RedisConfiguration.java
@@ -0,0 +1,33 @@
+package com.wh.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * redis配置
+ */
+@Configuration
+@Slf4j
+public class RedisConfiguration {
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        log.info("开始创建redis模板对象...");
+        try {
+            RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+            // 设置redis的连接工厂对象
+            redisTemplate.setConnectionFactory(redisConnectionFactory);
+            // 设置redis key的序列化器
+            redisTemplate.setKeySerializer(new StringRedisSerializer());
+            log.error("创建redis模板对象成功");
+            return redisTemplate;
+        } catch (Exception e) {
+            log.error("创建redis模板对象失败");
+            return null;
+        }
+    }
+}
