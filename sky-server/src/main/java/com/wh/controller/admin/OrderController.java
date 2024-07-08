57eebdecf31f6dd8a195331909d500fcf8b12d43
WH934426 <wh934426@outlook.com>
2024-07-08 23:36:14 +0800

    feat: 商户端查询订单详情功能实现

--- sky-server/src/main/java/com/wh/controller/admin/OrderController.java
+++ sky-server/src/main/java/com/wh/controller/admin/OrderController.java
@@ -9,6 +9,7 @@ import com.wh.vo.OrdersVO;
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -45,5 +46,16 @@ public class OrderController {
     public Result<OrderStatisticsVO> statisticsOrder() {
         OrderStatisticsVO orderStatisticsVO = orderService.statisticsOrder();
         return Result.success(orderStatisticsVO);
+    }
+
+    /**
+     * 订单详情
+     *
+     * @param id 订单id
+     * @return 订单详情
+     */
+    @GetMapping("/details/{id}")
+    public Result<OrdersVO> getOrderDetailById(@PathVariable Long id) {
+        return Result.success(orderService.getOrderDetailById(id));
     }
 }
