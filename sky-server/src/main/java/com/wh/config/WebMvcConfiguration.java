03a1306f571e71b75c28d83347d528d0ed576beb
WH934426 <wh934426@outlook.com>
2024-12-08 15:39:16 +0800

    fix(server): 在 admin 拦截器中排除 logout 路径

--- sky-server/src/main/java/com/wh/config/WebMvcConfiguration.java
+++ sky-server/src/main/java/com/wh/config/WebMvcConfiguration.java
@@ -34,7 +34,7 @@ public class WebMvcConfiguration extends WebMvcConfigurationSupport {
         log.info("开始注册自定义拦截器...");
         registry.addInterceptor(jwtTokenAdminInterceptor)
                 .addPathPatterns("/admin/**")
-                .excludePathPatterns("/admin/employee/login");
+                .excludePathPatterns("/admin/employee/login", "/admin/employee/logout");
         registry.addInterceptor(jwtTokenUserInterceptor)
                 .addPathPatterns("/user/**")
                 .excludePathPatterns("/user/user/login")
